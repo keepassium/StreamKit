77d05a782a354bbef532bec585eb3116d091b090
Ihar Katkavets <iharkatkavets@users.noreply.github.com>
2023-08-26 14:23:25 +0200

    main Update configuration for Local defined Package

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["StreamKit"]),
     ],
     dependencies: [
-        .package(path: "Core")
+        .package(name: "Core", path: "Core")
     ],
     targets: [
         .target(
